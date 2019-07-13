631158203a1bc5f8f99ce39bbd9d246a40ecc957
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-07-13 11:07:48 +0200

    Add @typescript-eslint support for TS projects

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,21 +1,41 @@
 const { ifAnyDep } = require('../utils');
 
 module.exports = {
+  parser: ifAnyDep(
+    ['babel-cli', 'babel-core', 'babel-eslint', 'babel-loader'], 'babel-eslint',
+    ifAnyDep('typescript', '@typescript-eslint/parser')
+  ),
+
   extends: [
-    // require.resolve('eslint-config-itp-base'), // not using because not yet updated to eslint5
     require.resolve('eslint-config-airbnb'),
+    // Dependencies
     ifAnyDep('jest', 'plugin:jest/recommended'),
     ifAnyDep('flow-bin', 'plugin:flowtype/recommended'),
     ifAnyDep('react-native', 'plugin:react-native/all'),
+    // Eslint + Prettier
     require.resolve('eslint-config-prettier'),
     ifAnyDep('flow-bin', 'eslint-config-prettier/flowtype'),
     require.resolve('eslint-config-prettier/react'),
   ].filter(Boolean),
-  parser: ifAnyDep(
-    ['babel-cli', 'babel-core', 'babel-eslint', 'babel-loader'],
-    'babel-eslint',
-    ifAnyDep(['typescript'], 'typescript-eslint-parser')
-  ),
+
+  plugins: [
+    ifAnyDep('typescript', '@typescript-eslint'),
+  ].filter(Boolean),
+
+  settings: {
+    "import/extensions": ['.js', '.jsx', ...ifAnyDep('typescript', ['.ts', '.tsx'])],
+    "import/resolver": {
+      node: {
+        extensions: ['.js', '.jsx', ...ifAnyDep('typescript', ['.ts', '.tsx'])]
+      }
+    },
+    ...ifAnyDep('typescript', {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"],
+      },
+    })
+  },
+
   rules: {
     ...ifAnyDep('react-native', { 'jsx-a11y/accessible-emoji': 'off' }),
     'no-use-before-define': [
@@ -28,10 +48,11 @@ module.exports = {
     'react/jsx-filename-extension': [
       'warn',
       {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', ifAnyDep('typescript', '.tsx')]
       },
     ],
   },
+
   globals: {
     fetch: false,
     alert: false,
